7d080c68fc3f35eb70ff8e16ebc0afc69a0904f7
sukrutureli <73479522+sukrutureli@users.noreply.github.com>
2025-09-17 09:47:51 +0300

    all updated

--- src/main/java/com/example/Scraper.java
+++ src/main/java/com/example/Scraper.java
@@ -3,8 +3,8 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -13,12 +13,14 @@ import java.util.List;
 
 public class Scraper {
     public static void main(String[] args) {
+        // GitHub Actions üzerinde ChromeDriver yolu ayarlanıyor
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
+        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+
         WebDriver driver = new ChromeDriver(options);
 
         try {
@@ -27,25 +29,31 @@ public class Scraper {
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
             List<WebElement> events = driver.findElements(By.cssSelector("div.odd-col.event-list.pre-event"));
-            System.out.println("Toplam event sayısı: " + events.size());
-            
+
             StringBuilder html = new StringBuilder();
             html.append("<!DOCTYPE html><html><head><meta charset='UTF-8'><title>IDDAA Bülteni</title></head><body>");
             html.append("<h1>IDDAA Güncel Bülteni</h1>");
 
             for (WebElement event : events) {
-                String matchName = event.findElement(By.cssSelector("div.name > a")).getText();
-                String matchTime = event.findElement(By.cssSelector("div.time > span")).getText();
+                try {
+                    WebElement nameEl = event.findElement(By.cssSelector("div.name > a"));
+                    String matchName = nameEl.getText();
 
-                List<WebElement> odds = event.findElements(By.cssSelector("dd.col-03.event-row .cell .odd"));
-                String odd1 = odds.size() > 0 ? odds.get(0).getText() : "-";
-                String oddX = odds.size() > 1 ? odds.get(1).getText() : "-";
-                String odd2 = odds.size() > 2 ? odds.get(2).getText() : "-";
+                    WebElement timeEl = event.findElement(By.cssSelector("div.time > span.passive-time"));
+                    String matchTime = timeEl.getText();
 
-                html.append("<div class='match'>")
-                    .append("<h3>").append(matchTime).append(" - ").append(matchName).append("</h3>")
-                    .append("<p>1: ").append(odd1).append(" | X: ").append(oddX).append(" | 2: ").append(odd2).append("</p>")
-                    .append("</div>");
+                    List<WebElement> odds = event.findElements(By.cssSelector("dd.col-03.event-row .cell .odd"));
+                    String odd1 = odds.size() > 0 ? odds.get(0).getText() : "-";
+                    String oddX = odds.size() > 1 ? odds.get(1).getText() : "-";
+                    String odd2 = odds.size() > 2 ? odds.get(2).getText() : "-";
+
+                    html.append("<div class='match'>")
+                        .append("<h3>").append(matchTime).append(" - ").append(matchName).append("</h3>")
+                        .append("<p>1: ").append(odd1).append(" | X: ").append(oddX).append(" | 2: ").append(odd2).append("</p>")
+                        .append("</div>");
+                } catch (Exception inner) {
+                    System.out.println("Bir maç parse edilirken hata: " + inner.getMessage());
+                }
             }
 
             html.append("<p>Güncelleme zamanı: ").append(java.time.LocalDateTime.now()).append("</p>");
@@ -58,7 +66,7 @@ public class Scraper {
                 fw.write(html.toString());
             }
 
-            System.out.println("✅ public/index.html başarıyla oluşturuldu!");
+            System.out.println("public/index.html başarıyla oluşturuldu!");
 
         } catch (Exception e) {
             e.printStackTrace();
