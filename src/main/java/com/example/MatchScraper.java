5fa85203a684dfac53fbd51b238d7156d17289de
sukrutureli <73479522+sukrutureli@users.noreply.github.com>
2025-09-20 00:19:09 +0300

    sdasd

--- src/main/java/com/example/MatchScraper.java
+++ src/main/java/com/example/MatchScraper.java
@@ -9,9 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.Duration;
+import java.time.*;
 import java.util.List;
 import java.util.ArrayList;
 
