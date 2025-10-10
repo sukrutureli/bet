7d5b35f840092f9fd4ba1df600f9fd8ed23c195d
sukrut <sukrut@sukrut.ayesas.web>
2025-10-10 18:07:30 +0300

    Anasayfa eklendi

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -76,7 +76,7 @@ public class Application {
                 results.add(ensemble.predict(m, Optional.empty()));
             }
             
-            HtmlReportGenerator.generateHtml(matches, historyManager, matchStats, results, "index.html");
+            HtmlReportGenerator.generateHtml(matches, historyManager, matchStats, results, "futbol.html");
             
         } catch (Exception e) {
             System.out.println("GENEL HATA: " + e.getMessage());
