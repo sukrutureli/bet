856380e842d35ef65ee9887617d8df2e5a37f230
Sukru Tureli <sukrutureli@gmail.com>
2025-11-13 15:32:10 +0300

    dsasd

--- src/main/java/com/example/model/TeamStats.java
+++ src/main/java/com/example/model/TeamStats.java
@@ -1,5 +1,8 @@
 package com.example.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TeamStats {
 
 	private String teamName;
